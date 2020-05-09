0fd2c6bc37835a1d97bf2e809aae414ffe90841a
Edney Baboim <ednney@gmail.com>
2020-05-09 19:53:59 -0300

    Design da view do Departamento

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -34,7 +34,7 @@ public class MainViewController implements Initializable{
 	
 	@FXML
 	public void onMenuItemDepartamentoAcao() {
-		System.out.println("onMenuItemDepartamentoAcao");
+		carregarView("/gui/ListaDepartamento.fxml");
 	}
 	
 	@FXML
