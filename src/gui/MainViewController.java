f166c2dce118580013e53bb0d4241cd029e728ae
Edney Baboim <ednney@gmail.com>
2020-05-10 10:19:11 -0300

    Implementação de inicialização como parâmetro

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,6 +3,7 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
@@ -35,13 +36,15 @@ public class MainViewController implements Initializable{
 	
 	@FXML
 	public void onMenuItemDepartamentoAcao() {
-		//carregarView("/gui/ListaDepartamento.fxml");
-		carregarView2("/gui/ListaDepartamento.fxml");
+		carregarView("/gui/ListaDepartamento.fxml", (ListaDepartamentoController controlador) -> {
+			controlador.setServicoDepartamento(new ServicoDepartamento());
+			controlador.atualizarTableView();
+		});
 	}
 	
 	@FXML
 	public void onMenuItemSobreAcao() {
-		carregarView("/gui/Sobre.fxml");
+		carregarView("/gui/Sobre.fxml", x -> {});
 	}
 	
 
@@ -49,25 +52,7 @@ public class MainViewController implements Initializable{
 	public void initialize(URL url, ResourceBundle rb) {
 	}
 	
-	private synchronized void carregarView(String nomeAbsoluto) {
-		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource(nomeAbsoluto));
-			VBox novoVBox = loader.load();
-			
-			Scene mainScene = Main.getMainScene();
-			VBox mainVbox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
-			
-			Node mainMenu = mainVbox.getChildren().get(0);
-			mainVbox.getChildren().clear();
-			mainVbox.getChildren().add(mainMenu);
-			mainVbox.getChildren().addAll(novoVBox.getChildren());	
-		}
-		catch (IOException e) {
-			Alerts.showAlert("IO Exception", "Erro ao carregar a view", e.getMessage(), AlertType.ERROR);
-		}
-	}
-	
-	private synchronized void carregarView2(String nomeAbsoluto) {
+	private synchronized <T> void carregarView(String nomeAbsoluto, Consumer<T> acaoDeInicializacao) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(nomeAbsoluto));
 			VBox novoVBox = loader.load();
@@ -80,13 +65,11 @@ public class MainViewController implements Initializable{
 			mainVbox.getChildren().add(mainMenu);
 			mainVbox.getChildren().addAll(novoVBox.getChildren());	
 			
-			ListaDepartamentoController controle = loader.getController();
-			controle.setServicoDepartamento(new ServicoDepartamento());
-			controle.atualizarTableView();
+			T controlador = loader.getController();
+			acaoDeInicializacao.accept(controlador);
 		}
 		catch (IOException e) {
 			Alerts.showAlert("IO Exception", "Erro ao carregar a view", e.getMessage(), AlertType.ERROR);
 		}
 	}
-
 }
